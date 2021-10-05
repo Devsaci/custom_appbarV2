8091a56366d401946eeb9ef1dba207237f0679c0
saci zakaria <sacideveloppeur@gmail.com>
2021-10-05 15:43:53 +0200

    barrierDismissible

--- lib/main.dart
+++ lib/main.dart
@@ -128,6 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
     );
     showDialog(
+        barrierDismissible: false,
         context: context,
         builder: (BuildContext context) {
           return alert;
