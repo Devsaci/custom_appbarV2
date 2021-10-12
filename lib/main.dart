e23f186cdbcca4c1d7accd208352a8c9ae9dc4b1
saci zakaria <sacideveloppeur@gmail.com>
2021-10-12 10:40:01 +0200

    backgroundColor/Icon

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         canvasColor: Colors.yellow,
       ),
       darkTheme:
-      ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.brown),
+          ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.brown),
       home: MyHomePage(),
     );
   }
@@ -86,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
         canvasColor: Colors.yellow,
       ),
       darkTheme:
-      ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.brown),
+          ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.brown),
       home: Scaffold(
         appBar: AppBar(
           leading: Padding(
@@ -127,31 +127,45 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ListView(
           children: const [
             ExpansionTile(
+              backgroundColor: Colors.pink,
+              leading: Icon(Icons.perm_identity),
               title: Text("Account"),
               children: [
-                Divider(color: Colors.black, thickness: 3,),
+                Divider(
+                  color: Colors.black,
+                  thickness: 3,
+                ),
                 ListTile(
+                  leading: Icon(Icons.arrow_back),
+                  trailing: Icon(Icons.add),
                   title: Text("Sign Up!"),
                   subtitle: Text("Where You can register An account"),
                 ),
                 ListTile(
+                  leading: Icon(Icons.arrow_back),
+                  trailing: Icon(Icons.add),
                   title: Text("Sign Up!"),
                   subtitle: Text("Where You can register An account"),
                 ),
-
               ],
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             ExpansionTile(
+              leading: Icon(Icons.phone_android),
               title: Text("More Information"),
               children: [
-                Divider(color: Colors.black, thickness: 3,),
+                Divider(
+                  color: Colors.black,
+                  thickness: 3,
+                ),
                 ListTile(
+                  leading: Icon(Icons.arrow_back),
+                  trailing: Icon(Icons.add),
                   title: Text("Contact"),
                   subtitle: Text("Where You can register An account"),
                 ),
-
-
               ],
             ),
           ],
@@ -482,7 +496,7 @@ class _MyHomePageState extends State<MyHomePage> {
           cursorWidth: 10,
           style: TextStyle(fontSize: 15),
           toolbarOptions:
-          ToolbarOptions(copy: true, selectAll: true, paste: true),
+              ToolbarOptions(copy: true, selectAll: true, paste: true),
         ),
         SizedBox(
           height: 15,
