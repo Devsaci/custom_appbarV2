d42cb57b869c0dafc40ecc6ecd01377efd4c6460
saci zakaria <sacideveloppeur@gmail.com>
2021-10-13 13:34:05 +0200

    PageView

--- lib/p_view.dart
+++ lib/p_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class PView extends StatefulWidget {
   const PView({Key? key}) : super(key: key);
 
@@ -11,6 +10,17 @@ class PView extends StatefulWidget {
 class _PViewState extends State<PView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: PageView(
+          children: [
+            Text("Page 1", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+            Text("Page 2", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+            Text("Page 3", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+          ],
+        ),
+      ),
+    );
   }
 }
