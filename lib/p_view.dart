d2dc767a0f4da3c17735974ff38f99292cabbf07
saci zakaria <sacideveloppeur@gmail.com>
2021-10-13 13:47:40 +0200

    Data construct

--- lib/p_view.dart
+++ lib/p_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 //Model Data class
-class Data{
+class Data {
+  late final String title;
+  late final String description;
+  late final String imageUrl;
+  late final IconData iconData;
 
+  Data(
+    this.title,
+    this.description,
+    this.imageUrl,
+    this.iconData,
+  );
 }
-
 
 class PView extends StatefulWidget {
   const PView({Key? key}) : super(key: key);
@@ -21,9 +30,18 @@ class _PViewState extends State<PView> {
       home: Scaffold(
         body: PageView(
           children: [
-            Text("Page 1", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            Text("Page 2", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            Text("Page 3", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+            Text(
+              "Page 1",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Page 2",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Page 3",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
