ff27cf9a07dc6f8fd6953f77c832619fdcec8a0c
saci zakaria <sacideveloppeur@gmail.com>
2021-10-14 09:59:00 +0200

    ExactAssetImage

--- lib/p_view.dart
+++ lib/p_view.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 //Model Data class
 class Data {
- late final String title;
- late final String description;
- late final String imageUrl;
- late final IconData iconData;
+  late final String title;
+  late final String description;
+  late final String imageUrl;
+  late final IconData iconData;
 
   Data(
     this.title,
@@ -23,11 +23,12 @@ class PView extends StatefulWidget {
 }
 
 class _PViewState extends State<PView> {
-  final  List<Data> myData = [
+  final List<Data> myData = [
     Data("title1", "description title1", "images/q1.jpg", Icons.add),
     Data("title2", "description title2", "images/q2.jpg", Icons.arrow_back),
     Data("title3", "description title3", "images/q3.jpg", Icons.phone_android),
-    Data("title4", "description title4", "images/q4.jpg", Icons.message_outlined),
+    Data("title4", "description title4", "images/q4.jpg",
+        Icons.message_outlined),
   ];
 
   @override
@@ -36,7 +37,15 @@ class _PViewState extends State<PView> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: PageView(
-          children: myData.map((item) => Container()).toList(),
+          children: myData
+              .map((item) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: ExactAssetImage(item.imageUrl),
+                      ),
+                    ),
+                  ))
+              .toList(),
         ),
       ),
     );
